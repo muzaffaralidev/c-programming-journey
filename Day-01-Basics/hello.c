1ff6b36662a8775f0bd49405c246d380db2b740f
Muzaffar Ali <kalwar12khan@gmail.com>
2026-09-09 21:01:16 +0500

    Day 01: C programming hello

---
+++ Day-01-Basics/hello.c
@@ -0,0 +1,11 @@
+void main(void){
+	// variable declaration
+	int num1,num2,result;
+	// variable intailization
+	num1 = 10;
+	num2 = 10;
+	result = num1 + num2;
+	
+	//display Output
+	printf("%d",result);
+}
