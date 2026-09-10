670a6faea4680e16d157300963469c3b01297d45
Muzaffar Ali <kalwar12khan@gmail.com>
2026-09-10 23:58:08 +0500

    day1 simplye calculation

---
+++ Day-01-Basics/calculator.c
@@ -0,0 +1,16 @@
+void main(void)
+{
+	float num1,num2, sum,multi,divide,subs;
+	num1 = 10;
+	num2 = 10;
+	
+	sum = num1 + num2;
+	multi = num1 * num2;
+	divide = num1 / num2;
+	subs = num1 - num2;
+	
+	printf("%.0f",sum);
+	printf("\n%.0f",multi);
+	printf("\n%.0f",divide);
+	printf("%.0f",subs);
+}
