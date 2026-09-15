0fa02749f053cce3213ef01c1c8a00366867c87e
Muzaffar Ali <kalwar12khan@gmail.com>
2026-09-15 12:40:18 +0500

    learn data types and some basics topics

---
+++ Day-01-Basics/basic.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+int main(void){
+    // Variables means memory location
+//     int number = 10;
+//     char star = '*';
+//     float pi = 3.14;
+//     int age = 21;
+//     int _age = 21;
+//     int total_price = 100;
+//     // int 2age = 21; // error
+
+// // varibes value are not fix but i can change thre value
+//      age = 33; // change age 
+
+     // Data Types
+    //  3 main data Types
+    //  int = integer :  examples : 0,1,3,-3,10,etc
+    // int age = 21;
+    // float pi = 3.14;
+    // char hashtag = '#';
+
+    // comments are 2 types
+    // single line comments --> // 
+    // this is a comments
+    // multiline comments
+    /*
+    this is a multiline comments
+     * ---> asterisk
+     # ---> Hastags
+    */
+    
+    // Output: printf is library function 
+    // printf("Hello C \n");
+    // printf("Hello C \n");
+    // printf("Hello C \n");
+    // printf("Hello C \n");
+
+    int age = 21;
+    printf("Age is %d \n",age); // formet spicifer %d
+
+    float pi = 3.14;
+    printf("age is %f \n",pi); // by default after point 6 digit in float 
+
+    char star = '*';
+    printf("star is %c",star);
+
+    // Input is library function 
+    // ampersand  &
+    // int age ;
+    // printf("Enter age ");
+    // scanf("%d",&age);
+    // printf("Age is %d",age);
+
+    // program 1  :   sum calculation using input 
+    int num1,num2;
+    printf("Enter First Number: ");
+    scanf("%d",&num1);
+    printf("Enter Second Number: ");
+    scanf("%d",&num2);
+    printf("total : %d",num1 + num2);
+    return 0;
+}
