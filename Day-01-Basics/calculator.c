c4831095c6fc14ec481dc1beb4907a1ca72bab50
Muzaffar Ali <kalwar12khan@gmail.com>
2026-09-16 12:22:00 +0500

    learn scanf for input

--- Day-01-Basics/calculator.c
+++ Day-01-Basics/calculator.c
@@ -1,18 +1,20 @@
-void main(void)
+#include <stdio.h>
+
+int main(void)
 {
-	float num1,num2, sum,multi,divide,subs;
-	num1 = 10;
-	num2 = 10;
-	
-	sum = num1 + num2;
-	multi = num1 * num2;
-	divide = num1 / num2;
-	subs = num1 - num2;
-	
-	printf("%.0f",sum);
-	printf("\n%.0f",multi);
-	printf("\n%.0f",divide);
-	printf("%.0f",subs);
+   int num1, num2,result;
+   printf("Enter First Number: ");
+   scanf("%d",&num1);
+   printf("Enter Secend Number: ");
+   scanf("%d",&num2);
+
+   result = num1 + num2;
+
+   printf("%d + %d = %d",num1,num2,result);
+ 	return 0;
 }
+
+
+
 
 
