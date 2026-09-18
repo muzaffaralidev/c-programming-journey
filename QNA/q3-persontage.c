4e30c45d2fe15be235b87cb88af915f585c9e9b6
Muzaffar Ali <kalwar12khan@gmail.com>
2026-09-18 12:08:15 +0500

    q3 solved persontage find

---
+++ QNA/q3-persontage.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// Qno1: Write a program to calculate area of a circle 
+
+int main(void){
+    float obt_mark,total_mark;
+    float persontage;
+    printf("Enter Your Marks: ");
+    scanf("%f",&obt_mark);
+    printf("Enter Total Marks: ");
+    scanf("%f",&total_mark);
+    persontage = (obt_mark / total_mark) * 100;
+    printf("Your persontage is : %f",persontage);
+
+    return 0;
+}
